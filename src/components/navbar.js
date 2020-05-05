2fcec6ad18a1f2b723b0cb2527850d57bb74e628
Marissol Karczeski <marfkar@gmail.com>
2020-05-05 10:53:15 -0700

    add components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Route, Link } from 'react-router-dom';
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../App.css';
 import axios from 'axios';
 
