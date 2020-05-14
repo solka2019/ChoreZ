4e5f4994171e46144aba3a8bc90b79f4f72a7dd9
Marissol <marfkar@gmail.com>
2020-05-13 23:23:34 -0700

    add dialogs to signup and child chore completed

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import ListTasks from "./ListTasks";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import AlertDialogSlide from './error-dialog';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 library.add(faTrash);
@@ -10,24 +12,43 @@ class Home extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            error: null,
+            message: null,
+            redirectTo: null
+        };
+
         this.onCompleteTask = this.onCompleteTask.bind(this);
+        this.onChildMessageClose = this.onChildMessageClose.bind(this);
+    }
+
+    onChildMessageClose()
+    {
+        // Force a reload of the page
+        // TODO: we want to reload only the tasks
+        this.setState({
+            redirectTo: '/'
+        });
     }
 
     onCompleteTask(id)
     {
-        alert("delete test");
         if(!id)
         {
             // didn't get a taskId, exit
             return;
         }
 
-        // Todo. Create an AJAX/AXIOS call to the server to set this task as completed
+        let that = this;
+
+        // Create an AJAX/AXIOS call to the server to set this task as completed
         // https://www.educative.io/edpresso/how-to-make-an-axios-post-request
         axios.post('/api/taskcompleted', {
             taskId: id
         }).then(function (response) {
-            alert("done test" + response);
+            that.setState({
+                message: "Yay!!! You finished another chore!"
+            });
         });
     }
 
@@ -38,32 +59,48 @@ class Home extends Component {
 
         console.log(this.props.appState.username);
 
-        if(this.props.appState.loggedIn){
-            // Parent home screen
-            return (
+        if (this.state.redirectTo) {
+            return ( 
                 <div>
-                <p> This is the main screen for our Parent management of the app</p>
-                {/* <img style={imageStyle} src="../resources/img/logono.png" /> */}
+                    <Redirect to={{ pathname: this.state.redirectTo }} />
+                </div>
+            )
+        } else {
+            if(this.props.appState.loggedIn){
+                // ----------------------
+                // Parent home screen
+                // ----------------------
 
-                { /* Only render the task list if there is a list from the server, and passed by the App component*/}
-                { this.props.appState.tasks && 
-                    <ListTasks items={this.props.appState.tasks} 
-                               onCompleteTask={this.onCompleteTask} /> }
-            </div>
-            );
-        }
-        else {
-            // Children home screen
-            return (
-                <div>
-                <p> This is the main screen for our Child Chores app</p>
-                {/* <img style={imageStyle} src="../resources/img/logono.png" /> */}
-                { this.props.appState.tasks && 
-                    <ListTasks items={this.props.appState.tasks} 
-                               onCompleteTask={this.onCompleteTask} /> }
-            </div>
-            );
-        }
+                return (
+                    <div>
+                    <p> This is the main screen for our Parent management of the app</p>
+                    {/* <img style={imageStyle} src="../resources/img/logono.png" /> */}
+
+                    { /* Only render the task list if there is a list from the server, and passed by the App component*/}
+                    { this.props.appState.tasks && 
+                        <ListTasks items={this.props.appState.tasks} 
+                                onCompleteTask={this.onCompleteTask} /> }
+                </div>
+                );
+            }
+            else {
+                // ----------------------
+                // Children home screen
+                // ----------------------
+
+                return (
+                    <div>
+                        <p> This is the main screen for our Child Chores app</p>
+                        {/* <img style={imageStyle} src="../resources/img/logono.png" /> */}
+                        { this.state.message && <AlertDialogSlide message={this.state.message} onClose={this.onChildMessageClose} title="Great job!"/>}
+
+                        { this.props.appState.tasks && 
+                            <ListTasks items={this.props.appState.tasks} 
+                                    onCompleteTask={this.onCompleteTask} /> }
+                    </div>
+                );
+            }
+        }      
     }
 }
 
--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import AlertDialogSlide from './error-dialog';
 
 class Signup extends Component {
 	constructor(props) {
@@ -8,11 +10,24 @@ class Signup extends Component {
 			username: '',
 			password: '',
 			confirmPassword: '',
-
+			redirectTo: null,
+			error: null,
+			registrationCompleted:false
 		};
 
+		this.onRegistrationCompleted = this.onRegistrationCompleted.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+
+	}
+
+	onRegistrationCompleted() {
+		if(this.state.registrationCompleted)
+		{
+			this.setState({ //redirect to login page
+				redirectTo: '/login'
+			});
+		}
 	}
 
 	handleChange(event) {
@@ -25,6 +40,10 @@ class Signup extends Component {
 		console.log('sign-up handleSubmit, username: ');
 		console.log(this.state.username);
 		event.preventDefault();
+		this.setState({
+			error: null,
+			redirectTo: null
+		});
 
 		//request to server to add a new parent's username/password
 		axios.post('/parent/', {
@@ -34,67 +53,82 @@ class Signup extends Component {
 			.then(response => {
 				console.log(response);
 				if (!response.data.error) {
-					console.log('successful signup')
-					this.setState({ //redirect to login page
-						redirectTo: '/login'
-					})
+					console.log('successful signup');
+					this.setState({
+						registrationCompleted: true
+					});
+					
 				} else {
-					console.log('username already taken');
+					this.setState({
+						error: "Username already taken!"
+					});
 				}
 			}).catch(error => {
 				console.log('signup error: ');
 				console.log(error);
-
+				this.setState({
+					error: error
+				});
 			});
 	}
 
-
 render() {
-	return (
-		<div className="SignupForm">
-			<h4>Sign up</h4>
-			<form className="form-horizontal">
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="username">Username</label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							type="text"
-							id="username"
-							name="username"
-							placeholder="Username"
-							value={this.state.username}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="password">Password: </label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							placeholder="password"
-							type="password"
-							name="password"
-							value={this.state.password}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group ">
-					<div className="col-7"></div>
-					<button
-						className="btn btn-primary col-1 col-mr-auto"
-						onClick={this.handleSubmit}
-						type="submit"
-					>Sign up</button>
-				</div>
-			</form>
-		</div>
+	if (this.state.redirectTo) {
+		return ( 
+			<div>
+				<Redirect to={{ pathname: this.state.redirectTo }} />
+			</div>
+		)
+	} else {
+		return (
+			<div className="SignupForm">
 
-	)
+				{ this.state.registrationCompleted  &&  <AlertDialogSlide onRegistrationCompleted={this.onClose} title="Registration completed" message="User created. Please, login now." />}
+				{ this.state.error  &&  <AlertDialogSlide title="Unable to register" message={this.state.error} />}
+				
+				<h4>Sign up</h4>
+				<form className="form-horizontal">
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="username">Username</label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								type="text"
+								id="username"
+								name="username"
+								placeholder="Username"
+								value={this.state.username}
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="password">Password: </label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								placeholder="password"
+								type="password"
+								name="password"
+								value={this.state.password}
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group ">
+						<div className="col-7"></div>
+						<button
+							className="btn btn-primary col-1 col-mr-auto"
+							onClick={this.handleSubmit}
+							type="submit"
+						>Sign up</button>
+					</div>
+				</form>
+			</div>
+		)
+	}
 }
 }
 
