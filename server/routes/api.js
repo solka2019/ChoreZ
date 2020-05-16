b6d14d118a526b68e8cda3cc49fba461b0ab5e32
Marissol <marfkar@gmail.com>
2020-05-16 14:48:58 -0700

    complete handling of child chore finished

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-//const Parent = require('../database/models/parent');
-//const Child = require('../database/models/child');
+const Parent = require('../database/models/parent');
+const Child = require('../database/models/child');
 const Task = require('../database/models/task');
 //const passport = require('../passport');
 
@@ -18,6 +18,10 @@ router.post('/taskcompleted',  (req, res) => {
         async (err, task) => {
             if (err) {
                 console.log('failed to find one task with id passed in and not completed!: ', err);
+                res.json({
+                    // todo: send something back to browser saying it worked
+                    success: false
+                });
             } else if (task) {
                 // todo: got the task, now we need to set it to 'complete: true'
                 console.log(task);
@@ -25,47 +29,100 @@ router.post('/taskcompleted',  (req, res) => {
                 await task.save();
                 res.json({
                     // todo: send something back to browser saying it worked
-                    task: task,
+                    success: true
+                });
+            }           
+        });
+});
+
+router.post('/taskupdated',  (req, res) => {
+    console.log('task updated api received a request from browser');
+
+    const idFromBrowser = req.body.task._id;
+    let task = req.body.task;
+
+    //https://kb.objectrocket.com/mongo-db/how-to-use-the-mongoose-findoneandreplace-function-1019
+    Task.findOneAndReplace({
+           _id: idFromBrowser
+        },
+        task,
+        (err, result) => {
+            if (err) {
+                console.log('failed to find one task with id passed in and not completed!: ', err);
+                res.json({
+                    // todo: send something back to browser saying it worked
+                    success: false
+                });
+            } else {
+                res.json({
+                    success: true
                 });
             }           
         });
 });
 
 
+
+// refreshTasksByParent from the App.js
+router.get('/tasksbyparent', async (req, res) => {
+    console.log('get all child tasks from a given parentId - will return every task completed or not');
+    const tasksArray = await Task.find({
+        parentId: req.parentId
+    });
+
+    console.log("Sending tasks to browser:");
+    console.log(JSON.stringify(tasksArray));
+    
+    res.json({
+        tasks: tasksArray
+    });
+});
+
+// refreshChildrenByParent from App.js
+router.get('/childrenbyparent', async (req, res) => {
+    console.log('get all children from a given parentId ');
+    const childrenArray = await Child.find({
+        parentId: req.parentId
+    });
+
+    console.log("Sending children to browser:");
+    console.log(JSON.stringify(childrenArray));
+    
+    res.json({
+        children: childrenArray
+    });
+});
+
+
+
+// refreshChildTasks - from the app.js
+router.get('/childtasks', async (req, res) => {
+    console.log('get all child tasks - will return the ones not completed');
+    const tasksArray = await Task.find({
+        completed: false
+    }); //kids only want to see the tasks they have not finished yet!
+
+    console.log("Sending tasks to browser:");
+    console.log(JSON.stringify(tasksArray));
+    
+    res.json({
+        tasks: tasksArray
+    });
+});
+
+
+
+// refreshChildren from App.js
+router.get('/children', async (req, res) => {
+    console.log('get all children ');
+    const childrenArray = await Child.find({});
+
+    console.log("Sending children to browser:");
+    console.log(JSON.stringify(childrenArray));
+    
+    res.json({
+        children: childrenArray
+    });
+});
+
 module.exports = router;
-
-// const router = require("express").Router();
-// const orm = require("../db/orm.js");
-
-// router.post("/api/task", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// module.exports = router;
--- src/App.js
+++ src/App.js
@@ -14,18 +14,81 @@ class App extends Component {
   constructor(props) {
     super(props);
     
+    this.getUser = this.getParentUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+    // this refresh the state of list based on a parentId for the parent page
+    this.refreshTasksByParent = this.refreshTasksByParent.bind(this);
+    this.refreshChildrenByParent = this.refreshChildrenByParent.bind(this);
+    // this refresh the state of lists that are used in a child page
+    this.refreshChildTasks = this.refreshChildTasks.bind(this);
+    this.refreshChildren = this.refreshChildren.bind(this);
+
     this.state = {
       loggedIn: false,
       username: null,
       name: null,
       children: [],
       tasks: [],
-      date: null
+      date: null,
+      refreshTasksByParent: this.refreshTasksByParent,
+      refreshChildrenByParent: this.refreshChildrenByParent,
+      refreshChildTasks: this.refreshChildTasks,
+      refreshChildren: this.refreshChildren
     };
 
-    this.getUser = this.getParentUser.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.updateUser = this.updateUser.bind(this);
+  }
+
+  refreshTasksByParent(id) {
+    axios.get('/api/tasksbyparent', {
+        parentId: id
+      }).then(response => {
+        console.log('Get api response: ');
+        console.log(response.data);
+      if (response.data.tasks) {
+        this.setState({
+          tasks: response.data.tasks,
+        });
+      }});
+  }
+
+  refreshChildrenByParent(id) {
+    axios.get('/api/childrenbyparent', {
+      parentId: id
+    }).then(response => {
+      console.log('Get api response: ');
+      console.log(response.data);
+    if (response.data.children) {
+      this.setState({
+        children: response.data.children,
+      });
+    }});
+  }
+
+  refreshChildTasks()
+  {
+    axios.get('/api/childtasks', {
+    }).then(response => {
+      console.log('Get api response: ');
+      console.log(response.data);
+    if (response.data.tasks) {
+      this.setState({
+        tasks: response.data.tasks,
+      });
+    }});
+  }
+
+  refreshChildren()
+  {
+    axios.get('/api/children', {
+    }).then(response => {
+      console.log('Get api response: ');
+      console.log(response.data);
+    if (response.data.children) {
+      this.setState({
+        children: response.data.children,
+      });
+    }});
   }
 
   componentDidMount() {
