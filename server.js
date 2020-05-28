14d69ff264b965bb7a4b96f573d187f92a829ce4
Marissol <marfkar@gmail.com>
2020-05-28 09:37:01 -0700

    fix attempt for cannot get / error in heroku

--- server.js
+++ server.js
@@ -23,17 +23,16 @@ app.use(
 
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV === 'production') {
-	// IN HEROKU
-	// console.log("Production build detected by NODE_ENV variable");
-	// app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+	// Exprees will serve up production assets
+	app.use(express.static('client/build'));
   
-	// const path = require('path');
-	// app.get('*', (req,res) => {
-	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	// });
-// }
-
+	// Express serve up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+  }
 
 // Sessions
 console.log("Creating a dev connection to mongoDB..");
